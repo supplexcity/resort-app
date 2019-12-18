b0e7f59012e1a625584a5616ddd01a906abf070d
Mohit Pahuja <mohitpahuja46@gmail.com>
2019-12-18 23:58:45 +0530

    minor changes

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,8 +1,5 @@
 import { createClient } from "contentful";
 
-console.log(process.env.REACT_APP_API_SPACE);
-console.log(process.env.REACT_APP_ACCESS_TOKEN);
-
 export default createClient({
   space: process.env.REACT_APP_API_SPACE,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN
